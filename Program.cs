ad03c4d2fd4122331bfaded25730ebbf55526040
Kübra Yılmaz <143116598+kubr-y@users.noreply.github.com>
2023-09-06 16:41:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Siparişinizin fiyatını giriniz: ");
+           int siparis=Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine("Siparişinize eklenecek bahşiş tutarını giriniz: ");
+           int bahsis=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Siparişinize eklenecek vergi tutarını giriniz: ");
+           int vergi=Convert.ToInt32(Console.ReadLine());
+
+           int sonuc1=siparis*bahsis/100;
+           int sonuc2=siparis*vergi/100;
+
+           double total=sonuc1+sonuc2+siparis;
+           Console.WriteLine("Toplam ödenecek tutar: "+total);
+           Console.WriteLine("-----------AFİYET OLSUN--------");
+
+
+        }
+    }
+}
